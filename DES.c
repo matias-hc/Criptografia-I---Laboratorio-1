fb06ff3c0ccaaac451a157c1e2626594c1f8c348
Benjamin Klerman Munoz <benjamin.klerman@gmail.com>
2020-05-27 20:41:08 -0400

    Prueba de funcion Expancion

--- DES.c
+++ DES.c
@@ -2,28 +2,52 @@
 #include <stdlib.h>
 
 
-int main(int argc, char *argv[]){
-    unsigned char ***S, **E, **P, it1, it2, it3;
-    S = (unsigned char ***)malloc(sizeof(unsigned char **)*8);
-    for (it1 = 0; it1 < 8; it1 += 1){
-    	S[it1] = (unsigned char **)malloc(sizeof(unsigned char *)*8);
-    	for (it2 = 0; it2 < 8; it2 += 1)
-    	    S[it1][it2] = (unsigned char *)malloc(sizeof(unsigned char));
-    }
-    printf("%d\n", it3);
-    FILE *f = fopen("tablas.dat", "r");
-    for (it1 = 0; it1 < 8; it1 += 1)
-        for (it2 = 0; it2 < 8; it2 += 1)
-            for (it3 = 0; it3 < 8; it3 += 1)
-                fscanf(f, "%d", &S[it1][it2][it3]);
-    
+unsigned char cpow(unsigned char b, unsigned char e){
+  unsigned char res=1;
+  while(e){
+    res*=b;
+    e-=1;
+  }
+  return res;
+}
 
-    return 0;
+unsigned char *Expansion(unsigned char *R);
+
+int main(int argc, char *argv[]){
+  unsigned char ***S, *E, **P, it1, it2, it3, *R;
+  S = (unsigned char ***)malloc(sizeof(unsigned char **)*8);
+  for (it1 = 0; it1 < 8; it1 += 1){
+    S[it1] = (unsigned char **)malloc(sizeof(unsigned char *)*8);
+    for (it2 = 0; it2 < 8; it2 += 1)
+      S[it1][it2] = (unsigned char *)malloc(sizeof(unsigned char));
+  }
+  //printf("%d\n", it3);
+  FILE *f = fopen("tablas.dat", "r");
+  for (it1 = 0; it1 < 8; it1 += 1)
+    for (it2 = 0; it2 < 8; it2 += 1)
+      for (it3 = 0; it3 < 8; it3 += 1)
+	fscanf(f, "%d", &S[it1][it2][it3]);
+    
+  R= (unsigned char *)malloc(sizeof(unsigned char)*4);
+  for(it1=0 ; it1<32 ; it1+=1){
+    R[0]=0;
+    R[1]=0;
+    R[2]=0;
+    R[3]=0;
+    R[it1/8]=cpow(2,it1%8);
+    E=Expansion(R);
+    if(!(it1%8))
+      printf("\n%d",it1/8);
+    printf("\n\n%d\n",R[it1/8]);
+    for(it2=0; it2<6 ; it2+=1)
+      printf("%d\t",E[it2]);
+  }
+  printf("\n");
+  return 0;
 }
 
 unsigned char *Expansion(unsigned char *R){
-   unsigned char *res;
-   int i;
+  unsigned char *res;
    res = (unsigned char *)malloc(sizeof(unsigned char)*6);
    res[0] = (R[3]%2)*128 + (R[0]/32)*16 + ((R[0]%32)/8)*(4+1);
    res[1] = ((R[0]%8/2)*64 + ((R[0]%2)*2+(R[1]/128))*(16+4) + ((R[1]%128)/32));
